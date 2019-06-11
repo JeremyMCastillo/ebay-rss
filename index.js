0ff76fec9ecc5d520bc672f270b2812822065b87
Jeremy Castillo <jeremy.c@accessorypower.com>
2019-06-11 14:30:35 -0700

    Initial Commit
    Signed-off-by: Jeremy Castillo <jeremy.c@accessorypower.com>

---
+++ index.js
@@ -0,0 +1,46 @@
+const Parser = require('rss-parser');
+
+function EbayRss(options) {
+  if (!(this instanceof EbayRss)) return new EbayRss(options);
+
+  options = options || {};
+
+  // Default pptions
+  this.options = {
+    baseSearchUrl: 'https://www.ebay.com/sch/i.html',
+    defaultCategory: '176985'
+  };
+
+  // Override options passed in
+  Object.keys(options).forEach((prop) => {
+    this.options[prop] = options[prop];
+  });
+}
+
+EbayRss.prototype = {
+  search(keywords, categoryId) {
+    if (!keywords) {
+      throw new Error('Keywords are missing.');
+    }
+    var categoryParameter = categoryId || this.options.defaultCategory;
+
+    var searchUrl = this.options.baseSearchUrl;
+    searchUrl += `?_nkw=${encodeURIComponent(keywords.toLowerCase())}`;
+    searchUrl += `&_sacat=${categoryParameter}&_rss=1`;
+
+    console.log('TCL: search -> searchUrl', searchUrl);
+    var parser = new Parser({
+      customFields: {
+        item: [
+          ['rx:BuyItNowPrice', 'BuyItNowPrice'],
+          ['rx:CurrentPrice', 'CurrentPrice'],
+          ['rx:EndTime', 'EndTime'],
+          ['rx:AuctionType', 'AuctionType']
+        ]
+      }
+    });
+    return parser.parseURL(searchUrl);
+  }
+};
+
+module.exports = EbayRss;
